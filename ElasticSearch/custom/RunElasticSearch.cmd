1054eec35fee24ac95d5c3c4668c74a03f8d9244
juanonsoftware <juanonsoftware@gmail.com>
2020-01-14 14:28:27 +0700

    fix home variable

--- ElasticSearch/custom/RunElasticSearch.cmd
+++ ElasticSearch/custom/RunElasticSearch.cmd
@@ -1,4 +1,4 @@
-PowerShell Copy-Item -Path C:\Custom\app\*.* -Destination %ELASTICSEARCH_SOURCE%\ -Recurse -Force -ErrorAction Ignore
+PowerShell Copy-Item -Path C:\Custom\app\*.* -Destination %ELASTICSEARCH_HOME%\ -Recurse -Force -ErrorAction Ignore
 
 PowerShell Copy-Item -Path C:\Custom\RunElasticSearch.cmd -Destination C:\ -Recurse -Force -ErrorAction Ignore
 
